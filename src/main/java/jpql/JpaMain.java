1450787b23bcc189d730d550eaf3a1f9d2044522
이아름 <areum6321@gmail.com>
2024-01-28 19:12:37 +0900

    JPQL - 파라미터 바인딩 이름기준 사용 코드

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -22,20 +22,12 @@ public class JpaMain {
             em.persist(member);
 
             //반환 타입이 명확할 때 사용
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-
-            //결과 조회 API
-            //1. 리스트 반환 - 결과가 1개 이상일 때
-            List<Member> resultList = query.getResultList();
-
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
-
-            //2. 단일 객체 반환 - 결과가 1개일 때
-            Member result = query.getSingleResult();
-            System.out.println("result = " + result);
+            Member result = em.createQuery("select m from Member m where m.username =:username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
             
+            System.out.println("singleResult = " + result.getUsername());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
