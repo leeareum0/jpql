c3049da26f20e581de0e85bdb80ef3eb5f1585aa
이아름 <areum6321@gmail.com>
2024-01-28 18:56:56 +0900

    JPQL - TypeQuery, Query 사용 코드

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,40 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            //반환 타입이 명확할 때 사용
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+
+            //반환 타입이 명확하지 않을 때 사용
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
