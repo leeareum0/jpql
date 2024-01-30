d028a1307236d2f4c9dd5a3cdc2026e80cddc564
이아름 <areum6321@gmail.com>
2024-01-30 16:58:37 +0900

    JPQL - 다대일 관계 페치 조인 사용 코드

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,25 +16,45 @@ public class JpaMain {
 
         try {
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            //사용자 정의 함수 호출
-            String query =
-                    "select function('group_concat', m.username) from Member m";
+            //페치 조인 - 다대일 관계
+            String query = "select m from Member m join fetch m.team";
 
-            List<String> result = em.createQuery(query, String.class).getResultList();
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                //회원1, 팀A(SQL)
+                //회원2, 팀A(1차 캐시)
+                //회원3, 팀B(SQL)
+
+                //회원 100명 -> N + 1
+
             }
 
             tx.commit();
