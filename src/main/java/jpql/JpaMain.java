3bc6d4d7c644a3a4effc2bbcf0d918ddb9c660ac
이아름 <areum6321@gmail.com>
2024-01-30 21:02:24 +0900

    JPQL - 벌크 연산 사용 코드

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,7 +1,6 @@
 package jpql;
 
 import javax.persistence.*;
-import java.util.List;
 
 public class JpaMain {
 
@@ -26,29 +25,31 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
+            //FLUSH
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                            .executeUpdate();
+
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            Member findMember = em.find(Member.class, member1.getId());
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
